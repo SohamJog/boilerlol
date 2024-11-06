f6b0f19e3b2e02a0a6a20e9a170a04c538ad0062
Soham Jog <jogsoham2003@gmail.com>
2024-11-06 16:37:27 -0500

    todo

--- src/utils/html_embdeddings/gradescope.rs
+++ src/utils/html_embdeddings/gradescope.rs
@@ -6,20 +6,17 @@ pub fn construct_gradescope_html(query: &str) -> Result<String, Box<dyn Error>> 
     // Strip 'gr ' from the query string
     let cmd = &query[3..];
 
-    // Fetch the HTML of the Gradescope courses page
     let url = "https://www.gradescope.com/";
+    // TODO: find a way to search the browser so we'll already be logged in
     let response = get(url)?.text()?;
 
-    // Parse the HTML content
     let document = Html::parse_document(&response);
     println!("Document: {:?}", document);
 
-    // Define the selector to extract course names and URLs
-    let course_selector = Selector::parse(".courseBox").unwrap(); // Adjust selector if needed
-    let name_selector = Selector::parse(".courseBox--name").unwrap(); // For course name
+    let course_selector = Selector::parse(".courseBox").unwrap(); 
+    let name_selector = Selector::parse(".courseBox--name").unwrap(); 
     let link_selector = Selector::parse("a").unwrap(); // To get the URL
 
-    // Find the closest match
     let closest_match = find_closest_match(
         &document,
         cmd,
@@ -28,7 +25,6 @@ pub fn construct_gradescope_html(query: &str) -> Result<String, Box<dyn Error>> 
         &link_selector,
     )?;
 
-    // Return HTML to redirect to the closest course
     Ok(format!(
         r#"
         <!DOCTYPE html>
@@ -47,14 +43,12 @@ pub fn construct_gradescope_html(query: &str) -> Result<String, Box<dyn Error>> 
     ))
 }
 
-// Struct to hold course info
 #[derive(Debug)]
 struct Course {
     name: String,
     url: String,
 }
 
-// Function to search for the closest matching course
 fn find_closest_match(
     document: &Html,
     cmd: &str,
@@ -65,9 +59,7 @@ fn find_closest_match(
     let mut closest_match: Option<Course> = None;
     let mut min_distance = usize::MAX;
 
-    // Iterate over each course element
     for course_elem in document.select(course_selector) {
-        // Get the course name
         let name = course_elem
             .select(name_selector)
             .next()
@@ -75,7 +67,6 @@ fn find_closest_match(
             .text()
             .collect::<String>();
 
-        // Get the link (URL)
         let link = course_elem
             .select(link_selector)
             .next()
@@ -84,10 +75,8 @@ fn find_closest_match(
             .attr("href")
             .unwrap_or("");
 
-        // Calculate a simple measure of "closeness" (e.g., substring match)
         let distance = calculate_distance(&name, cmd);
 
-        // Update closest match if this is a better match
         if distance < min_distance {
             closest_match = Some(Course {
                 name: name.clone(),
@@ -100,7 +89,7 @@ fn find_closest_match(
     closest_match.ok_or_else(|| "No matching course found.".into())
 }
 
-// Simple function to calculate "closeness" (you can use more advanced algorithms like Levenshtein distance)
+
 fn calculate_distance(course_name: &str, cmd: &str) -> usize {
     course_name.to_lowercase().contains(&cmd.to_lowercase()) as usize
 }
